eba1b7cb5ca596a646aca1724362423a848be3c2
Mitheel Ramdaw <106301219+mitheelramdaw@users.noreply.github.com>
2024-04-09 13:08:20 +0200

    routing

--- airstream/main/views.py
+++ airstream/main/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("<h1> home page</h1>")
+
+def about(response):
+    return HttpResponse("<h1> about page </h1>")
+
+def contact(response):
+    return HttpResponse("<h1> contact page </h1>")
